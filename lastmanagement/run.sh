91d86605235e7b84342fbcd6dff5f61ffeacd2b1
Christoph Woerndl <christoph.woerndl@redbull.com>
2026-05-24 22:43:04 +0200

    3

--- lastmanagement/run.sh
+++ lastmanagement/run.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/with-contenv bashio
 echo "Starte Lastmanagement-Add-on..."
 
-# Lade Konfiguration
 CONFIG_PATH=/data/options.json
 LOG_LEVEL=$(bashio::config 'log_level')
 UPDATE_INTERVAL=$(bashio::config 'update_interval')
 
-# Starte das Python-Skript
-python3 /app/lastmanagement.py --config /data/options.json --log-level $LOG_LEVEL --interval $UPDATE_INTERVAL
+exec python3 /app/lastmanagement.py --config "$CONFIG_PATH" --log-level "$LOG_LEVEL" --interval "$UPDATE_INTERVAL"
